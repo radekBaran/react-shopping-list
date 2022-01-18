b2745b94056bde59ee9cf1df72cc02704ac9c5e5
R4d0 <bradek@onet.pl>
2022-01-18 22:29:49 +0100

    switch to prod properties

--- src/http-common.tsx
+++ src/http-common.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
-  //   baseURL: "https://reactnotesbackend-rb.herokuapp.com/api",
+  //   baseURL: "http://localhost:8080/api",
+  baseURL: "https://task-manager-rb.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
